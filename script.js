e89c3463c8625929cd210482efc349f4bcf676d7
Snorl4xX7 <anatoleayadi@gmail.com>
2024-02-16 22:31:34 +0900

    Added logic to players and score

--- script.js
+++ script.js
@@ -1,36 +1,64 @@
+const player1Name = document.getElementById("player1-Name");
+const player2Name = document.getElementById("player2-Name");
+
 const player1Score = document.getElementById("player1-Score");
 const player2Score = document.getElementById("player2-Score");
+
 let player1 = "streamelements";
 let player2 = "stoney_eagle";
-let p1Score = 0;
-let p2Score = 0;
 
-const incrementScore = (player) => {
-  if (player === player1) {
-    p1Score += 1;
-    player1Score.innerText = p1Score;
-  } else {
-    p2Score += 1;
-    player2Score.innerText = p2Score;
-  }
+const players = [
+  {
+    name: "StreamElements",
+    user: "streamelements",
+    score: 0,
+  },
+  {
+    name: "Stoney_Eagle",
+    user: "stoney_eagle",
+    score: 0,
+  },
+  {
+    name: "aaoa_",
+    user: "aaoa_",
+    score: 0,
+  },
+];
+
+const updateScore = () => {
+  players.sort((a, b) => b.score - a.score);
+  const chatters = players.slice(0, 2);
+  chatters.map((chatter, index) => {
+    if (index === 0) {
+      player1Name.innerText = chatter.name;
+      player1Score.innerText = chatter.score;
+    } else {
+      player2Name.innerText = chatter.name;
+      player2Score.innerText = chatter.score;
+    }
+  });
 };
 
-setTimeout(() => {
-  incrementScore(player2);
-}, 3000);
+const check = (username) => {
+  const player = players.find((player) => player.user == username);
+  if (player) {
+    player.score += 1;
+  }
+  updateScore();
+};
 
 const client = new tmi.Client({
   options: { debug: true },
-  //   identity: {
-  //     username: "bot_name",
-  //     password: "oauth:my_bot_token",
-  //   },
   channels: ["aaoa_"],
 });
 client.connect().catch(console.error);
 client.on("message", (channel, tags, message, self) => {
   if (self) return;
   if (tags.username === player1 || tags.username === player2) {
-    incrementScore(tags.username);
+    check(tags.username); // new function check
   }
 });
+
+// function playerCheck(tags.username) {
+
+// }
