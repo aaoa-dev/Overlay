09fdc2f2412442ea5c70dc93121da71c913e1ed2
Snorl4xX7 <anatoleayadi@gmail.com>
2024-03-14 20:43:05 +0900

    Code cleanup

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
 import {reset, check} from './commands/index.js';
 
-
-// Manual reset if Raid fail
-// const manualReset = () =>
-// {localStorage.removeItem("players");
-//  console.log('Local Storage cleared');
-//  players.length = 0;
-//  resetUI();
-// };
-
-// manualReset();
-
-// const buttonReset = () => {
-//   addEventListener("click", this.onclick)
-//   localStorage.removeItem("players"); // but not this
-//   console.log('Local Storage cleared'); // this works
-//   players.length = 0;
-//   resetUI();
-// };
-
 //This is TMI stuff that check message and act accordingly
 const client = new tmi.Client({
   options: { debug: true },
