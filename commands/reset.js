5d50c57c06c7d311636ca9acd94bbdfdcac2d63c
aaoa <anatoleayadi@gmail.com>
2025-05-05 09:33:53 +0200

    may be overlay update

--- commands/reset.js
+++ commands/reset.js
@@ -6,22 +6,6 @@ const p3Name = document.getElementById("p3-Name");
 const p1Score = document.getElementById("p1-Score");
 const p2Score = document.getElementById("p2-Score");
 const p3Score = document.getElementById("p3-Score");
-const p1Avatar = document.getElementById("p1-Avatar");
-const p2Avatar = document.getElementById("p2-Avatar");
-const p3Avatar = document.getElementById("p3-Avatar");
-
-function getColorFromUsername(username) {
-  // Simple hash function to generate a color
-  let hash = 0;
-  for (let i = 0; i < username.length; i++) {
-    hash = username.charCodeAt(i) + ((hash << 5) - hash);
-  }
-  // Convert to RGB
-  const r = (hash & 0xFF0000) >> 16;
-  const g = (hash & 0x00FF00) >> 8;
-  const b = hash & 0x0000FF;
-  return `rgb(${r}, ${g}, ${b})`;
-}
 
 export const updateScore = () => {
   players.sort((a, b) => b.score - a.score);
@@ -30,25 +14,19 @@ export const updateScore = () => {
   // Reset all displays first
   [p1Name, p2Name, p3Name].forEach(el => el.innerText = "N/A");
   [p1Score, p2Score, p3Score].forEach(el => el.innerText = "0");
-  [p1Avatar, p2Avatar, p3Avatar].forEach(el => {
-    el.innerText = "";
-    el.style.backgroundColor = "#4c1d95"; // Default violet color
-  });
 
   // Update with available players
   topThree.forEach((chatter, index) => {
     const elements = [
-      [p1Name, p1Score, p1Avatar],
-      [p2Name, p2Score, p2Avatar],
-      [p3Name, p3Score, p3Avatar]
+      [p1Name, p1Score],
+      [p2Name, p2Score],
+      [p3Name, p3Score]
     ][index];
 
     if (elements) {
-      const [nameEl, scoreEl, avatarEl] = elements;
+      const [nameEl, scoreEl] = elements;
       nameEl.innerText = chatter.name;
       scoreEl.innerText = chatter.score;
-      avatarEl.innerText = chatter.name[0].toUpperCase();
-      avatarEl.style.backgroundColor = getColorFromUsername(chatter.user);
     }
   });
 };
@@ -58,10 +36,6 @@ updateScore();
 export const resetUI = () => {
   [p1Name, p2Name, p3Name].forEach(el => el.innerText = "N/A");
   [p1Score, p2Score, p3Score].forEach(el => el.innerText = "0");
-  [p1Avatar, p2Avatar, p3Avatar].forEach(el => {
-    el.innerText = "";
-    el.style.backgroundColor = "#4c1d95"; // Default violet color
-  });
 };
 
 export let isRaiding = false;
